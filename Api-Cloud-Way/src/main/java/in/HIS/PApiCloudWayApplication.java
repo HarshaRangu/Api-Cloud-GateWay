212481931cf1c90921ddf4994f4c3c8356e7675b
Harsha <satyampsharsha@gmail.com>
2023-10-13 10:46:55 +0530

    Api-Cloud-GateWay

---
+++ Api-Cloud-Way/src/main/java/in/HIS/PApiCloudWayApplication.java
@@ -0,0 +1,13 @@
+package in.HIS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PApiCloudWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PApiCloudWayApplication.class, args);
+	}
+
+}
---
+++ Api-Cloud-Way/src/test/java/in/HIS/PApiCloudWayApplicationTests.java
@@ -0,0 +1,13 @@
+package in.HIS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PApiCloudWayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
